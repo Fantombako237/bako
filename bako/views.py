842e6d22465b62e614e3c9cddecb2a94ff7f76f7
Bakonda <Denzibakonda@gmail.com>
2024-06-18 16:31:22 +0100

    bakonda created a view and edited the admin.py

--- bako/views.py
+++ bako/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Recipe
+from .serializers import CategorySerializer, RecipeSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
