9e561f2836ff57b0a25f8a5a9b2f7f5b22fa4b58
Bakonda <Denzibakonda@gmail.com>
2024-06-18 16:40:29 +0100

    bakonda added tests for the app

--- bako/tests.py
+++ bako/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Recipe, Category
 
-# Create your tests here.
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.category = Category.objects.create(name='Dinner')
+        self.recipe = Recipe.objects.create(
+            user=self.user,
+            title='Test Recipe',
+            description='Test Description',
+            ingredients='Test Ingredients',
+            instructions='Test Instructions',
+            category=self.category
+        )
+
+    def test_recipe_creation(self):
+        recipe = Recipe.objects.get(title='Test Recipe')
+        self.assertEqual(recipe.description, 'Test Description')
+
+    def test_category_assignment(self):
+        recipe = Recipe.objects.get(title='Test Recipe')
+        self.assertEqual(recipe.category.name, 'Dinner')
